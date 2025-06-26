bbe699d97f09d1e6a43bd3ec020ffb86c8ad39d2
dzakyfzl <160983103+dzakyfzl@users.noreply.github.com>
2025-06-26 11:41:21 +0700

    konfigurasi backend

---
+++ server.js
@@ -0,0 +1,47 @@
+/*
+    LIBRARY YANG DIGUNAKAN
+    - express: untuk membuat server dan mengatur routing
+    - path: untuk mengelola path file
+*/
+const express = require('express') //Express
+const path = require('path')
+
+
+/*
+    MENYEDIAKAN FILE STATIS
+    contoh : css, js,gambar, dll pada folder src
+*/
+app.use('/',express.static(path.join(__dirname, 'src')))
+
+
+/*
+    ENDPOINT UNTUK PAGE
+*/
+// 1. index atau halaman utama
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'))
+})
+
+
+/* 
+    ENDPOINT UNTUK API
+    endpoint harus diawali dengan /api
+    contoh: /api/rokok
+*/
+// 1. API untuk data jumlah perokok di indonesia
+app.get('/api/perokok', (req, res) => {
+    res.json({
+        jumlah: 60000000, // Isi dengan jumlah perokok di Indonesia
+        tahun: 2023 // Isi dengan tahun data tersebut
+    })
+})
+
+/*
+    KONFIGURASI SERVER
+    - Gunakan port yang telah ditentukan
+    - Gunakan express.static untuk menyajikan file statis
+*/
+//Memulai Server
+app.listen(port,()=>{
+    console.log(`server start in http://localhost:${port}`)
+})
